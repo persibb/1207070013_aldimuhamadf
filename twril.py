820882b56699b5061a6d78b923b0a0731d979d51
persibb <133184273+persibb@users.noreply.github.com>
2023-06-12 23:00:42 +0700

    Add files via upload

---
+++ twril.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt 
+from skimage import data 
+from skimage.transform import swirl 
+from skimage import io
+ 
+image = io.imread('2.jpg')
+swirled = swirl(image, rotation=0, strength=10, 
+radius=4000) 
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, 
+figsize=(8, 3), 
+ sharex=True, 
+sharey=True) 
+ax0.imshow(image, cmap=plt.cm.gray) 
+ax0.axis('off') 
+ax1.imshow(swirled, cmap=plt.cm.gray) 
+ax1.axis('off') 
+plt.show()
